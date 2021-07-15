375075477e91768a71169da3a53b3c6fa749cf44
nifz <mochanifz@gmail.com>
2021-07-15 23:14:07 +0700

    updated route

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['reset'=>false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
