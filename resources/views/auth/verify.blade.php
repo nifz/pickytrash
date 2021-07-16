e4fcde4b1a3e8fc9e83aa559ada758290bf3df74
nifz <mochanifz@gmail.com>
2021-07-16 21:08:13 +0700

    updated verification link

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -15,7 +15,10 @@
                         @endif
                         <p>Before proceeding, please check your email for a verification link.If you did not receive
                             the email,</p>
-                        <a href="{{ route('verification.resend') }}">click here to request another'</a>.
+                            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                                @csrf
+                                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                            </form>
                     </div>
                 </div>
             </div>
