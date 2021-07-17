b3ee94aa6eb4a0ef3e4ef45a526f078e7b5b4f7e
nifz <mochanifz@gmail.com>
2021-07-17 15:45:15 +0700

    updated seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,61 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $create_user = User::create([
+            'name' => 'Mochammad Hanif',
+            'email' => 'me@hanifz.com',
+            'password' => Hash::make('qweqwe123'),
+            'role' => 3,
+        ]);
+
+        Type::create([
+            'type' => 'Botol Plastik',
+            'price' => 100,
+            'image' => 'img/sampah/botolplastik.png',
+        ]);
+        Type::create([
+            'type' => 'Kardus',
+            'price' => 50,
+            'image' => 'img/sampah/kertaskarton.png',
+        ]);
+        Type::create([
+            'type' => 'Botol Kaca',
+            'price' => 100,
+            'image' => 'img/sampah/botolkaca.png',
+        ]);
+        Type::create([
+            'type' => 'Kertas Koran',
+            'price' => 50,
+            'image' => 'img/sampah/kertaskoran.png',
+        ]);
+        
+        TypeBank::create([
+            'name' => "BCA",
+        ]);
+        TypeBank::create([
+            'name' => "OVO",
+        ]);
+
+        StatusSell::create([
+            'name' => 'Mengajukan Penjualan Sampah.',
+        ]);
+        StatusSell::create([
+            'name' => 'Menerima Pengajuan Penjualan Sampah.',
+        ]);
+        StatusSell::create([
+            'name' => 'Menuju Perjalanan.',
+        ]);
+        StatusSell::create([
+            'name' => 'Telah Sampai Ditempat Tujuan.',
+        ]);
+        StatusSell::create([
+            'name' => 'Menerima Sampah.',
+        ]);
+        StatusSell::create([
+            'name' => 'Pengajuan Penjualan Sampah Telah Diterima.',
+        ]);
+        StatusSell::create([
+            'name' => 'Pengajuan Tidak Sesuai, Penjualan Dibatalkan.',
+        ]);
     }
 }
