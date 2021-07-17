ad3c590a0a4477dcb229303fc9bfa164cde14198
nifz <mochanifz@gmail.com>
2021-07-17 16:53:52 +0700

    updated LocationController

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laravolt\Indonesia\Models\Province;
+use Laravolt\Indonesia\Models\City;
+use Laravolt\Indonesia\Models\District;
+use Laravolt\Indonesia\Models\Village;
 
 class LocationController extends Controller
 {
     //
+    public function province_store(Request $request)
+    {
+        $cities = Province::pluck('name', 'id', 'meta');
+
+        return response()->json($cities);
+    }
+    public function city_store(Request $request)
+    {
+        $cities = City::where('province_id', $request->get('id'))
+            ->pluck('name', 'id', 'meta');
+
+        return response()->json($cities);
+    }
+
+    public function district_store(Request $request)
+    {
+        $district = District::where('city_id', $request->get('id'))
+            ->pluck('name', 'id', 'meta');
+
+        return response()->json($district);
+    }
+
+    public function village_store(Request $request)
+    {
+        $village = Village::where('district_id', $request->get('id'))
+            ->pluck('name', 'id', 'meta');
+
+        return response()->json($village);
+    }
 }
