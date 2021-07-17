83ddc94447643d6d5214d69c60bd6112253634ad
nifz <mochanifz@gmail.com>
2021-07-17 18:14:34 +0700

    updated login pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 @endsection
 @section('content')
     <div class="card card-primary">
-        <div class="card-header"><h4>Admin Login</h4></div>
+        <div class="card-header"><h4>Login</h4></div>
 
         <div class="card-body">
             <form method="POST" action="{{ route('login') }}">
