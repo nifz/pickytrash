8f3a65df8e7ba62010570e26add94905da20d062
nifz <mochanifz@gmail.com>
2021-07-17 16:27:50 +0700

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,51 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
+
+Route::group(['middleware'=>'isUser'],function(){
+    Route::prefix('user')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('user.dashboard');
+        Route::post('/', [UserController::class, 'store'])->name('user.store');
+        Route::get('/withdrawal', [HomeController::class, 'withdrawal'])->name('user.withdrawal');
+        Route::post('/withdrawal', [HomeController::class, 'withdrawal_store'])->name('user.withdrawal.store');
+        Route::get('/settings', [HomeController::class, 'settings'])->name('user.settings');
+        Route::post('/settings', [HomeController::class, 'settings_store'])->name('user.settings.store');
+        Route::post('/types', [UserController::class, 'types_store'])->name('user.types.store');
+        Route::post('/profile_account_address', [UserController::class, 'profile_account_address_store'])->name('user.profile_account.address.store');
+        Route::get('/kocak/{id}', [UserController::class, 'profile_account_address_kocak'])->name('user.profile_account.address.kocak');
+    });
+});
+
+Route::group(['middleware'=>'isDriver'],function(){
+    Route::prefix('driver')->group(function () {
+        Route::get('/', [DriverController::class, 'index'])->name('driver.dashboard');
+        Route::post('/', [DriverController::class, 'store'])->name('driver.store');
+        Route::get('/withdrawal', [HomeController::class, 'withdrawal'])->name('driver.withdrawal');
+        Route::post('/withdrawal', [HomeController::class, 'withdrawal_store'])->name('driver.withdrawal.store');
+        Route::get('/settings', [HomeController::class, 'settings'])->name('driver.settings');
+        Route::post('/settings', [HomeController::class, 'settings_store'])->name('driver.settings.store');
+    });
+});
+
+Route::group(['middleware'=>'isAdmin'],function(){
+    Route::prefix('admin')->group(function () {
+        Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
+        Route::post('/', [AdminController::class, 'store'])->name('admin.store');
+        Route::get('/settings', [HomeController::class, 'settings'])->name('admin.settings');
+        Route::post('/settings', [HomeController::class, 'settings_store'])->name('admin.settings.store');
+        Route::get('/garbages', [AdminController::class, 'garbage'])->name('admin.garbage');
+        Route::post('/garbages', [AdminController::class, 'garbage_store'])->name('admin.garbage_store');
+        Route::get('/banks', [AdminController::class, 'banks'])->name('admin.banks');
+        Route::post('/banks', [AdminController::class, 'banks_store'])->name('admin.banks_store');
+        Route::get('/statsells', [AdminController::class, 'status_sells'])->name('admin.status_sells');
+        Route::post('/statsells', [AdminController::class, 'status_sells_store'])->name('admin.status_sells_store');
+        Route::prefix('accounts')->group(function () {
+            Route::get('/', [AdminController::class, 'list_account'])->name('admin.list_account');
+            Route::get('/create', [AdminController::class, 'create_account'])->name('admin.create_account');
+            Route::post('/create', [AdminController::class, 'create_account_store'])->name('admin.create_account.store');
+            Route::get('/{id}', [AdminController::class, 'profile_account'])->name('admin.profile_account');
+            Route::post('/{id}', [AdminController::class, 'profile_account_store'])->name('admin.profile_account_store');
+            Route::post('/{id}/profile_account_address', [AdminController::class, 'profile_account_address_store'])->name('admin.profile_account.address.store');
+        });
+    });
+});
