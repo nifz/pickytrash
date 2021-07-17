534dd330a5cdd5cc1a5a520325eaa047e819cddd
nifz <mochanifz@gmail.com>
2021-07-18 02:44:52 +0700

    updated landing pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -501,19 +501,19 @@
                         @csrf
                     <div class="form-group">
                         <label for="name">Nama</label>
-                        <input type="text" name="name" id="name" class="form-control" style="background-color: transparent; border: black 1px solid; border-radius: 0;" placeholder="Masukkan nama" aria-describedby="name">
+                        <input type="text" name="name" id="name" class="form-control" style="background-color: transparent; border: black 1px solid; border-radius: 0;" placeholder="Masukkan nama" aria-describedby="name" required>
                     </div>
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="text" name="email" id="email" class="form-control" style="background-color: transparent; border: black 1px solid; border-radius: 0;" placeholder="Masukkan email" aria-describedby="email">
+                        <input type="text" name="email" id="email" class="form-control" style="background-color: transparent; border: black 1px solid; border-radius: 0;" placeholder="Masukkan email" aria-describedby="email" required>
                     </div>
                     <div class="form-group">
                         <label for="subject">Subject</label>
-                        <input type="text" name="subject" id="subject" class="form-control" style="background-color: transparent; border: black 1px solid; border-radius: 0;" placeholder="Masukkan subject" aria-describedby="subject">
+                        <input type="text" name="subject" id="subject" class="form-control" style="background-color: transparent; border: black 1px solid; border-radius: 0;" placeholder="Masukkan subject" aria-describedby="subject" required>
                     </div>
                     <div class="form-group">
                         <label for="message">Pesan</label>
-                        <textarea name="message" id="message" class="form-control" style="background-color: transparent; border: black 1px solid; border-radius: 0;" placeholder="Masukkan pesan" aria-describedby="message" rows="3"></textarea>
+                        <textarea name="message" id="message" class="form-control" style="background-color: transparent; border: black 1px solid; border-radius: 0;" placeholder="Masukkan pesan" aria-describedby="message" rows="3" required></textarea>
                     </div>
                     <button class="btn btn-grey">Kirim Pesan</button>
                     </form>
