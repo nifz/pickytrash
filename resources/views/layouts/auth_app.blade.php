3633fc276d99dfc795d365585e0c89d2a08c1661
nifz <mochanifz@gmail.com>
2021-07-17 18:13:46 +0700

    updated layouts auth

--- resources/views/layouts/auth_app.blade.php
+++ resources/views/layouts/auth_app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>@yield('title') | {{ config('app.name') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/logo_square.png') }}">
 
     <!-- General CSS Files -->
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
