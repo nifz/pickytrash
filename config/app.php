44da20a625b74752fc5748a13c3aeeebc07919fd
nifz <mochanifz@gmail.com>
2021-07-17 15:42:02 +0700

    added Wallet providers

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         App\Providers\RoleServiceProvider::class,
+        App\Providers\WalletServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -230,6 +231,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Role' => App\Helpers\Role::class,
+        'Wallet' => App\Helpers\Wallet::class,
 
     ],
 
