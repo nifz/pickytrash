e40bd0917797fad4dd65bd0e720510f0c9de4b6e
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-07-17 23:10:23 +0700

    Create ContactUs.php

---
+++ app/Models/ContactUs.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactUs extends Model
+{
+    use HasFactory;
+
+    protected $table = "contact_us";
+    protected $fillable = [
+        'name',
+        'email',
+        'subject',
+        'message',
+        'is_reply',
+    ];
+}
