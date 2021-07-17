bbe219410437fdb67a4edd9d39b9ec68524d7532
nifz <mochanifz@gmail.com>
2021-07-17 15:54:11 +0700

    update Kernel routeMiddleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,5 +62,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'isUser' => \App\Http\Middleware\UserMiddleware::class,
+        'isDriver' => \App\Http\Middleware\DriverMiddleware::class,
+        'isAdmin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
