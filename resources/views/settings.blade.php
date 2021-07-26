c997a02bd97c282ddcfed7f4bff0e778f82026d4
nifz <mochanifz@gmail.com>
2021-07-26 21:19:37 +0700

    updated settings

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title','Peraturan')
+@section('title','Pengaturan')
 @section('content')
 <div class="container-fluid">
     <div class="row justify-content-center">
